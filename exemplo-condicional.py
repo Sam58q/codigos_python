8338643f75858899ae06ce8fe62303e208c095cf
Samuel AD <araujodiassamuel6@gmail.com>
2024-09-04 08:27:50 -0300

    Adicionando Condicionais

---
+++ exemplo-condicional.py
@@ -0,0 +1,10 @@
+valor = int(input("Informe um valor numérico:"))
+
+if valor >= 50: 
+    print(f"o número {valor} é maior ou igual a 50")
+elif valor >= 30 and valor<50:
+    print(f"O número {valor} está entre 30 e 50\n")     
+else:
+    print(f"o numero {valor} é menor que 30\n")
+    
+    
